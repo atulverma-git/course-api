e470e5aee0d87e65fb3459b711032f9c6f1c0646
atulverma-git <atulverma.515@gmail.com>
2019-12-15 07:43:32 +0530

    test file added

--- src/main/java/io/springbootstarter/topic/TopicService.java
+++ src/main/java/io/springbootstarter/topic/TopicService.java
@@ -8,34 +8,36 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("Spring","Spring Framwork","Spring Framwork Desc"),
-									   new Topic("Java","Core Java","Core Java Desc"),
-									   new Topic("JavaScript","Java Script","Java Script Desc")));
-	
-	public List<Topic> getAllTopics(){
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(
+					new Topic("Spring", "Spring Framwork", "Spring Framwork Desc"),
+					new Topic("Java", "Core Java", "Core Java Desc"),
+					new Topic("JavaScript", "Java Script", "Java Script Desc")));
+
+	public List<Topic> getAllTopics() {
 		return topics;
 	}
-	
+
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
-	
+
 	public void addTopic(Topic topic) {
 		topics.add(topic);
 	}
 
 	public void updateTopic(Topic topic, String id) {
-		for(int i=0; i<topics.size();i++) {
+		for (int i = 0; i < topics.size(); i++) {
 			Topic obj = topics.get(i);
-			if(obj.getId().equals(id)) {
+			if (obj.getId().equals(id)) {
 				topics.set(i, topic);
 				return;
 			}
 		}
 	}
-	
+
 	public void deleteTopic(String id) {
-		topics.removeIf(t->t.getId().equals(id));
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 
 }
